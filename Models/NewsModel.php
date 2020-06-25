b1cdaf07051b680919e2cd05f8e2259fb2859da0
DANG ANH QUOC <aquoc13.s@gmail.com>
2020-06-25 17:26:02 +0700

    fixed NewsModel

--- Models/NewsModel.php
+++ Models/NewsModel.php
@@ -1,21 +1,22 @@
 <?php
-    
-    require 'DBConnect.php';
 
-    class NewsModel{
-        
-        public function getAllNews(){
-            $db = new db();
-            $result = $db->query('Select * FROM news');
-            return $db->fetchAll($result);
-        }
+require 'DBConnect.php';
 
-        //Take one News
-        public function getNews($Id){
-            $db = new db();
-            $result = $db->query("SELECT FROM news WHERE 'N_Id'='" .$Id. "'");
-            return $result;
-        }
+class NewsModel
+{
+
+    public function getAllNews()
+    {
+        $db = new db();
+        $result = $db->query('Select * FROM news');
+        return $db->fetchAll($result);
     }
 
-?>
+    //Take one News
+    public function getNews($Id)
+    {
+        $db = new db();
+        $query = "SELECT * FROM news WHERE N_id=?";
+        return $db->fetchOne($query, $Id);
+    }
+}
