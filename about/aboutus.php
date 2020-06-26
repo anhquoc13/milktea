853e2b511aa06d50f8150d05857d833967275fe6
DANG ANH QUOC <aquoc13.s@gmail.com>
2020-06-27 03:35:47 +0700

    potato

---
+++ about/aboutus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About us</title>
+</head>
+<body >
+
+<img src="wallpaper.jpg" style="height: 100%; width: 100%">
+<h1 style="position: absolute; left: 43%;top: 80px; color: white;font-family:Courier New;">ABOUT US</h1>
+<div style="position: absolute; left: 40px; top: 200px;">
+	
+<?php include('vude.php')  ?>
+
+<div>
+<div style="position: absolute; left: 310px;top: 0px;">
+	
+<?php include('quocanh.php')  ?>
+
+<div>
+<div style="position: absolute; left: 310px; top: 0px;">
+	
+<?php include('chidinh.php')  ?>
+
+<div>
+<div style="position: absolute; left: 310px; top: 0px;">
+	
+<?php include('tienem.php')  ?>
+
+<div>
+
+</body>
+</html>
---
+++ about/chidinh.php
@@ -0,0 +1,58 @@
+<!-- Trong đoạn Code này, bạn có thể thay đổi các chi tiết (tên riêng của bạn, của riêng bạn avatar vv), nhưng không làm thay đổi cấu trúc cơ bản! -->
+
+<aside class="profile-card">
+
+  <header>
+    <link rel="stylesheet" type="text/css" href="css.css">
+    <!-- Hình của bạn -->
+    <a href="https://www.facebook.com/aquoc13">
+      <img src="https://scontent.fvca1-2.fna.fbcdn.net/v/t1.0-1/p240x240/81151810_1054484074884305_5444482234411122688_n.jpg?_nc_cat=101&_nc_sid=dbb9e7&_nc_oc=AQmI40gYB1tpKzRQUZIGNpTBaouJw-zmBwnl2y_nOEQV4fOAGKrRSSQGeMvr-lkTy6U&_nc_ht=scontent.fvca1-2.fna&_nc_tp=6&oh=422afea9597a53481bd629f718d5a1ea&oe=5F1CD5DD">
+    </a>
+
+    <!-- Tên của bạn -->
+    <h1>Đặng Quốc Anh</h1>
+
+    <!-- Công việc hay nghề của bạn -->
+    <h2>- Web Developer -</h2>
+
+  </header>
+
+  <!-- Thêm thông tin của bạn -->
+  <div class="profile-bio">
+
+    <p>Chào mừng các bạn</p>
+    <p>Tôi là một nhà phát triển web. Tôi chủ yếu làm việc với PHP, HTML, CSS, JS và WordPress.
+      <br />Tôi cũng làm việc tốt với Photoshop, Corel Draw, After Effects và vài thứ khác.</p>
+
+  </div>
+
+  <!-- Liên kết mạng xã hội -->
+  <ul class="profile-social-links">
+
+    <!-- twitter - el clásico  -->
+    <li>
+      <a href="http://twitter.com/">
+        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-twitter.svg">
+      </a>
+    </li>
+
+    <!-- envato – use this one to link to your marketplace profile -->
+    <li>
+      <a href="https://www.facebook.com/aquoc13">
+        <img src="http://farm5.staticflickr.com/4221/34651248062_73641b1a80_o.png">
+      </a>
+    </li>
+
+    <!-- Tài khoản Google +-->
+    <li>
+      <a href="https://plus.google.com/u/0/118260931574775251745/posts">
+        <img src="http://farm5.staticflickr.com/4186/34004419773_ed97040ef5_o.png">
+      </a>
+    </li>
+
+    <!--Bạn có thể thêm hoặc xóa mạng xã hội tại đây-->
+
+  </ul>
+
+</aside>
+<!-- Chỉ có vậy thôi ! -->
