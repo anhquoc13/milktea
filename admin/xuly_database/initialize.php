c0271b56beb7bdf06938e8a371238ccd10c0819d
DANG ANH QUOC <aquoc13.s@gmail.com>
2020-06-27 01:31:40 +0700

    admin page

--- admin/xuly_database/initialize.php
+++ admin/xuly_database/initialize.php
@@ -2,18 +2,16 @@
 
 defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 
-defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.'milktea');
+defined('SITE_ROOT') ? null : define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . DS . 'milktea');
 
-defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'admin/xuly_database');
+defined('LIB_PATH') ? null : define('LIB_PATH', SITE_ROOT . DS . 'admin/xuly_database');
 
-require_once(LIB_PATH.DS."config.php");
-require_once(LIB_PATH.DS."function.php");
-require_once(LIB_PATH.DS."session.php");
-require_once(LIB_PATH.DS."accounts.php");
-require_once(LIB_PATH.DS."products.php");
-require_once(LIB_PATH.DS."categories.php");
-require_once(LIB_PATH.DS."locations.php");
+require_once LIB_PATH . DS . "config.php";
+require_once LIB_PATH . DS . "function.php";
+require_once LIB_PATH . DS . "session.php";
+require_once LIB_PATH . DS . "accounts.php";
+require_once LIB_PATH . DS . "products.php";
+require_once LIB_PATH . DS . "categories.php";
+require_once LIB_PATH . DS . "locations.php";
 
-
-require_once(LIB_PATH.DS."database.php");
-?>
+require_once LIB_PATH . DS . "database.php";
