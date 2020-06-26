4086eb37bac0d9da324fe5706f1b3c9d6cbb3824
Ryan <ryan@datadrone.com>
2020-06-27 03:27:44 +0700

    about page template

--- Templates/About.php
+++ Templates/About.php
@@ -3,9 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
+    <title>About</title>
+    <link rel="stylesheet" href="./public/css/News.css">
 </head>
 <body>
-    about
+    <?php include './Templates/Header.php'?>
+<div class="banner">
+About This page
+</div>
+    <div class="container">
+    </div>
+    <?php include './Templates/Footer.php'?>
 </body>
 </html>
