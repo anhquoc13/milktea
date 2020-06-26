430edd7a55c393dfde1eca63d7442b938d966660
DANG ANH QUOC <aquoc13.s@gmail.com>
2020-06-26 09:30:12 +0700

    updated style for news details

--- Templates/NewsArticle.php
+++ Templates/NewsArticle.php
@@ -1,22 +1,78 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>List News</title>
-    <link rel="stylesheet" href="./public/css/News.css">
-</head>
-<body>
-    <?php include './Templates/Header.php'?>
 
-    <div class="container">
-            <div class="col" >
-            <img src="<?php echo $newsArticle['imageS'] ?>">
-            <h4><?php echo $newsArticle['title'] ?></h4>
+    <style type="text/css">
+        *{
+            margin:0;
+            padding:0;
+        }
+        .outside{
+            background-image:url(./source/image/background-details.jpeg);
+            display:flex;
+            height:100%;
+            width:100%;
 
-            </div>
+        }
+        .containerDetails{
+            margin-top:40px;
+            margin-bottom: 40px;
+            margin-left:auto;
+            margin-right:auto;
+            padding: 20px 10px 0 10px;
+            width: 700px;
+            min-height:auto;
+            background-color: white;
+        }
+        img{
+            margin-top: 10px;
+            margin-bottom: 10px;
+            padding-left:10%;
+            width:80%;
+            height: auto;
+        }
+
+        p{
+            margin: 0 20px;
+            line-height:1.5rem;
+            text-align: justify;
+        }
+
+        .time p{
+            text-align:right;
+            font-weight:bold;
+        }
+        .close {
+        position: absolute;
+        top: 25px;
+        right: 330px;
+        color: #f1f1f1;
+        font-size: 40px;
+        font-weight: bold;
+        transition: 0.3s;
+        }
+
+
+        a:link{
+            font-weight:bold
+            color:white;
+            text-decoration:none;
+        }
+
+        .close a:visited{
+            color:white;
+        }
+
+    </style>
+
+
+    <div class="outside">
+        <div class="containerDetails">
+        <h3><?php echo $newsArticle['title'] ?></h3>
+        <hr>
+            <img src="<?php echo $newsArticle['imageB'] ?>">
+            <p><?php echo $newsArticle['body'] ?></p>
+        <div class="time">
+        <p><?php echo $newsArticle['NgayBD'] ?> | <?php echo $newsArticle['NgayKT'] ?></p>
+
+        </div>
+        <div class="close"><a href="./?action=news">&times;</div>
     </div>
-
-    <?php include './Templates/Footer.php'?>
-</body>
-</html>
+    </div>
